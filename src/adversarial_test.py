726a4f32139863927bc4ea388ecec513bf4b1070
Cruiz102 <cruiznavarro44@gmail.com>
2024-02-09 20:48:10 -0400

    added more parameters in the attacks training file.

--- src/adversarial_test.py
+++ src/adversarial_test.py
@@ -6,17 +6,14 @@
 # files 
 
 import matplotlib.pyplot as plt
-
+from transformers import AutoModelForImageClassification, AutoConfig, TrainingArguments, Trainer, AutoModel
 import torchattacks as ta
 from utils import read_yaml
 import argparse 
 
 # Read the yaml config file of the configuration of the attacks
-
+# import tqdm
 attacks_config = read_yaml("./configs/attacks.yaml")
-
-
-pgd_attack = ta.PGD(model, _)
 
 
 
@@ -36,19 +33,57 @@ def main():
 
     # Read the yaml config file of the configuration of the attacks
     attacks_config = read_yaml("./configs/attacks.yaml")
+    local_checkpoint = attacks_config["model"]["local_model_path"]
+    hugginface_model = attacks_config["model"]["hugginface_model"]
+    if local_checkpoint and hugginface_model:
+        raise Exception("""Choose either of the of the following configurations.
+                 Local checkpoint and hugginface_model has been specified. Only one should be given""")
+    if local_checkpoint:
+        model = AutoModelForImageClassification()
 
     #Load the model from the checkpoint 
     model = AutoModelForImageClassification.from_pretrained(args.model_checkpoint)
-
-    enable_one_pixel_attack = attacks_config["one_pixel"]["enable_attack"]
     enable_cw = attacks_config["carlini_wiener"]["enable_attack"]
-    enable_PGD = attacks_config["PGD"]["enable_attack"]
+    
 
+
+    # One Pixel Attack Parameters
+    enable_one_pixel_attack = attacks_config["one_pixel"]["enable_attack"]
+    onepixel_steps = attacks_config["one_pixel"]["steps"]
+    population = attacks_config["one_pixel"]["population_size"]
+    num_pixels = attacks_config["one_pixel"]["pixels"]
+
+
+
+    #  Carlini Weiner Parameters
+    enable_cw = attacks_config["carlini_wiener"]["enable_attack"]
+    steps_cw = attacks_config["carlini_wiener"]["steps"]
+    kappa_cw = attacks_config["carlini_wiener"]["kappa"]
+
+
+
+    # PGD Parameters
+    enable_PGD = attacks_config["PGD"]["enable_attack"]
+    steps_pgd = attacks_config["PGD"]["steps"]
 
     if enable_one_pixel_attack:
-        one_pixel_attack = ta.OnePixelAttack(model, _)
+        one_pixel_attack = ta.OnePixel(model= model,pixels= num_pixels, steps= onepixel_steps, popsize= population)
+
+    if enable_cw:
+        cw_attack = ta.CW(model=model, steps=steps_cw)
+
+    if enable_PGD:
+        pgd_attack = ta.PGD(model, steps=steps_pgd)
 
 
+    
+
+
+
+if __name__ == "__main__":
+    main()
+
+    
 
 
 
